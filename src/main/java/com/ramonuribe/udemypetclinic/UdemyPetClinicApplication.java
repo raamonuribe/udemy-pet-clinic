b798575800496b93b8b49f23c7f0bd103cdde4ed
Ramon <ramon@ramonuribe.com>
2021-02-22 16:06:34 -0700

    Initialized Project

---
+++ src/main/java/com/ramonuribe/udemypetclinic/UdemyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ramonuribe.udemypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramonuribe/udemypetclinic/UdemyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonuribe.udemypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
